8b9fcedba9b3bf8754b4e197badbf75d24e522da
Mariana Mendes <filhatronixmmlima@gmail.com>
2022-07-18 14:12:12 -0300

    model completa e tabela do mysql

---
+++ modulo6/case-wirecard/src/models/Payment.ts
@@ -0,0 +1,17 @@
+export type CreditPayment = {
+    id: string,
+    amount: number,
+    payment: string,
+    credit_card: string,
+    payment_situation: string,
+    user_id: string
+}
+
+export type TicketPayment = {
+    id: string,
+    amount: number,
+    payment: string,
+    payment_situation: string,
+    id_tickert: string,
+    user_id: string
+}
