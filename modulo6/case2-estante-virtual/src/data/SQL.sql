46211a6add530a18903a7952b2d63b0f45af60d4
Mariana Mendes <filhatronixmmlima@gmail.com>
2022-07-25 15:32:05 -0300

    MySQL

---
+++ modulo6/case2-estante-virtual/src/data/SQL.sql
@@ -0,0 +1,20 @@
+-- Active: 1656509809949@@35.226.146.116@3306@silveira-21814389-mariana-lima
+CREATE TABLE competition (
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    date VARCHAR(255) NOT NULL,
+    status VARCHAR (255) NOT NULL
+);
+
+SELECT * FROM competition;
+
+CREATE TABLE result (
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
+    competition VARCHAR(255) NOT NULL,
+    athlete VARCHAR(255) NOT NULL,
+    value DOUBLE NOT NULL,
+    unit ENUM('s', 'm') NOT NULL,
+    FOREIGN KEY (competition) REFERENCES competition(name)
+);
+
+select * from result;
