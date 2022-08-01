28d0c164f7d08f4e98a59b07535b945e428f3644
Mariana Mendes <filhatronixmmlima@gmail.com>
2022-08-01 17:14:28 -0300

    Services, Type, Model, BaseDatabase e Tabela do MySQL

---
+++ modulo6/case3-amaro/src/services/Authenticator.ts
@@ -0,0 +1,15 @@
+import * as jwt from "jsonwebtoken";
+import { AuthenticationData } from "../types/AuthenticationData";
+
+export class Authenticator { 
+    generateToken = (id: AuthenticationData): string => {
+        return jwt.sign(
+            id,
+            "r2d2c3po",
+            {expiresIn: "1h"}
+        )}
+
+    getTokenData = (token:string): AuthenticationData => {
+        return jwt.verify(token, "r2d2c3po") as AuthenticationData
+    }
+} 
---
+++ modulo6/case3-amaro/src/types/DTO/LoginInputDTO.ts
@@ -0,0 +1,4 @@
+export interface LoginIpuntDTO {
+    email: string,
+    password: string
+}
