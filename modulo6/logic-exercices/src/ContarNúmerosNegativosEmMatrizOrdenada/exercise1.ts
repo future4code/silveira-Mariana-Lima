7bb5364a505f35ec74e09b2f182fa9bf1feb844f
Mariana Mendes <filhatronixmmlima@gmail.com>
2022-08-12 10:54:06 -0300

    Contar números negativos em matriz ordenada

---
+++ modulo6/logic-exercices/src/ContarNúmerosNegativosEmMatrizOrdenada/exercise1.ts
@@ -0,0 +1,20 @@
+let a = 0;
+let b = 0;
+let letter = 0;
+let column = 0;
+let ans = 0;
+
+const negatives:any = (matriz:any) => {
+    a=matriz.length,b=matriz[0].length, letter=0, column=b - 1,ans=0;
+        while (letter < a) {
+        while (column >= 0 && matriz[letter][column] < 0) column--;
+        ans += b - column - 1;
+        letter++;
+        }
+    return ans; 
+};
+
+console.log(negatives([[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]));
+console.log(negatives([[3,2],[1,0]]));
+console.log(negatives([[1,-1],[-1,-1]]));
+console.log(negatives([[-1]]));
